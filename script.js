c81558ed92b8a7ef98ebbdad4d4aa9b7b806da85
Brennan Doehring <brennandoehringd@outlook.com>
2020-09-14 04:43:11 -0400

    Added full day forecast using moment.js and OpenWeatherMap API.

--- script.js
+++ script.js
@@ -1,32 +1,84 @@
 $(document).on("ready", function() {
     // Loads search history data for cities
     
+});
 
-    // Initiates the city search on search button click
-    $("#citySubmit").on("click", function() {
-        var cityInput = $("#cityName").val();
-        console.log(cityInput + " was the city entered!");
-        var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=61bd6110a742b7f64e0d6364d6a196d9";
+// Initiates the city search on search button click
+$("#citySubmit").on("click", function() {
+    console.log("Search button clicked!");
+    var cityInput = $("#cityInp").val();
+    console.log(cityInput + " was the city entered!");
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=61bd6110a742b7f64e0d6364d6a196d9";
+    // Grabs the information from the OpenWeatherMap API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log ("Ajax is returning data.");
+        // Turns response into usable variables
+        var cityNameDay = response.city.name;
+        var temperatureDay = k2f(response.list[0].main.temp);
+        var humidityDay = response.list[0].main.humidity + "%";
+        var windSpeedDay = response.list[0].wind.speed + "MPH";
+        var iconDay = response.list[0].weather[0].icon;
+        var latitude = response.city.coord.lat;
+        var longitude = response.city.coord.lon;
 
-        // Grabs the information from the OpenWeatherMap API
+        // Logs results
+        console.log(response);
+        console.log("City name: " + cityNameDay);
+        console.log("Temperature: " + temperatureDay);
+        console.log("Humidity: " + humidityDay);
+        console.log("Wind Speed: " + windSpeedDay);
+        console.log("Icon: " + iconDay);
+        console.log("Latitude: " + latitude + " Longitude: " + longitude);
+
+        // Get current day
+        const currentDate = moment().format("MM/DD/YYYY");
+        console.log("Today's date is: " + currentDate);
+        
+        // Gets UV Index and displays
+        var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude
+        + "&appid=61bd6110a742b7f64e0d6364d6a196d9" + "&cnt=1";
+
         $.ajax({
-            url: queryURL,
+            url: uvQueryURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
-            console.log("City name: " + response.city.name);
-            console.log("Temperature: " + response.list[0].main.temp);
-            console.log("Humidity: " + response.list[0].wind.speed);
-            console.log("");
-            // Response is placed into html for user readability
-            
-        })
+            var uvIndex = response[0].value;
+            $("#uvIndex").text("UV Index: " + uvIndex);
+        });
 
-    // Kelvin to fahrenheit conversion
+        // Displays the remaining variables
+        $("#cityName").text(cityNameDay + "(" + currentDate + ")");
+        $("#icon").attr("src" , "https://openweathermap.org/img/wn/" + iconDay + "@2x.png");
+        $("#temperature").text("Temperature: " + temperatureDay);
+        $("#humidity").text("Humidity: " + humidityDay);
+        $("#windSpeed").text("Wind Speed: " + windSpeedDay);
 
+        // FORECAST BEGINS HERE
+        // DAY ONE
+        // var cityNameDay = response.city.name;
+        // var temperatureDay = k2f(response.list[1].main.temp);
+        // var humidityDay = response.list[1].main.humidity + "%";
+        // var windSpeedDay = response.list[1].wind.speed + "MPH";
+        // var iconDay = response.list[1].weather[1].icon;
+        // var latitude = response.city.coord.lat;
+        // var longitude = response.city.coord.lon;
 
-    // Five day forecast
+        // const currentDate = new Date(response.list[0].dt * 1000);
+        // const day = currentDate.getDate();
+        // const month = currentDate.getMonth() + 1;
+        // const year = currentDate.getFullYear();
+        // console.log(currentDate);
 
-    // Local storage
-    })  
+})
+
+// Kelvin to fahrenheit conversion
+    function k2f(k) {
+        return Math.floor((k - 273.15) * 1.8 + 32);
+    }
+
+// Local storage
 });
